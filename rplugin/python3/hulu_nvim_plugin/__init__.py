35a8d47e9d91a6411de00d4c1b2ab239311f8901
nations-Xiong <nations19991117@outlook.com>
2024-05-20 11:29:07 +0800

    import answer from hulu_nvim_plugin

--- rplugin/python3/hulu_nvim_plugin/__init__.py
+++ rplugin/python3/hulu_nvim_plugin/__init__.py
@@ -1,1 +1,13 @@
-import hulu_nvim_plugin
+from hulu_nvim_plugin import ANSWER 
+
+import pynvim
+
+
+@pynvim.plugin
+class Plugin(object):
+    def __init__(self, nvim):
+        self.nvim = nvim
+
+    @pynvim.command('Test')
+    def test(self):
+        self.nvim.command(f"echo 'answer from hulu_nvim_plugin: {ANSWER}'")
--- rplugin/python3/hulu_nvim_plugin/hulu_nvim_plugin.py
+++ rplugin/python3/hulu_nvim_plugin/hulu_nvim_plugin.py
@@ -1,11 +1,1 @@
-import pynvim
-
-
-@pynvim.plugin
-class Plugin(object):
-    def __init__(self, nvim):
-        self.nvim = nvim
-
-    @pynvim.command('Test')
-    def test(self):
-        self.nvim.command('echo "hello from hulu_nvim_plugin"')
+ANSWER = 'yes'
