f18b997452b4fa3e28b2ca9a0d6c9f8b206890db
nations-Xiong <nations19991117@outlook.com>
2024-05-20 12:59:07 +0800

    print range

--- rplugin/python3/hulu_nvim_plugin/__init__.py
+++ rplugin/python3/hulu_nvim_plugin/__init__.py
@@ -11,3 +11,8 @@ class Plugin(object):
     @pynvim.command('Test')
     def test(self):
         self.nvim.command(f"echo 'answer from hulu_nvim_plugin: {ANSWER}'")
+
+    @pynvim.command('PrintRange')
+    def print_range(self):
+        for line in self.nvim.current.range:
+            print(line)
