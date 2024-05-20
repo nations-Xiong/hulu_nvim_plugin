1f600bfbf2db6aa114d2b26d96905bfdd2869e31
nations-Xiong <nations19991117@outlook.com>
2024-05-20 11:00:07 +0800

    update

--- rplugin/python3/hulu_nvim_plugin.py
+++ rplugin/python3/hulu_nvim_plugin.py
@@ -6,6 +6,6 @@ class Plugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @pynvim.command('Test')
-    def test(self, args):
-        self.nvim.command('echo "test"')
+    @pynvim.command('Test', nargs='*', range='')
+    def test(self, args, range):
+        self.nvim.out_write('hello from hulu_nvim_plugin')
